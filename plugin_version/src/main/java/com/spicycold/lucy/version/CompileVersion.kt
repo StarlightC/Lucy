3487d76f4f37df2ee47dc54e18c23c39d9e334b9
StarlightC <mail.starlightc@gmail.com>
2023-09-14 14:10:49 +0800

    Upgrade compile and Target SDK version

--- plugin_version/src/main/java/com/spicycold/lucy/version/CompileVersion.kt
+++ plugin_version/src/main/java/com/spicycold/lucy/version/CompileVersion.kt
@@ -16,9 +16,9 @@ object CompileVersion {
     val Java = JavaVersion.VERSION_17
     const val JVM = "17"
 
-    const val CompileSDK = 33
+    const val CompileSDK = 34
     const val MinAndroidSDK = 27
-    const val TargetSDK = 33
+    const val TargetSDK = 34
     const val CMake = "3.22.1"
     const val KotlinCompilerExtension = "1.4.3"
 }
